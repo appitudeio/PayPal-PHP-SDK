7314fd4a0445ee140053d5ac342e1ebc61ec8770
Oakleaf <mathiaseklof@gmail.com>
2024-04-14 22:04:04 +0200

    Update Plan.php

--- lib/PayPal/Api/Plan.php
+++ lib/PayPal/Api/Plan.php
@@ -336,10 +336,10 @@ class Plan extends PayPalResourceModel
         $payLoad = "";
         $allowedParams = array(
             'page_size' => 1,
-            'status' => 1,
+            'plan_ids' => 1,
             'page' => 1,
             'total_required' => 1,
-            'product_id' => null
+            'product_id' => 1
         );
         $json = self::executeCall(
             "/v1/billing/plans/" . "?" . http_build_query(array_intersect_key($params, $allowedParams)),
